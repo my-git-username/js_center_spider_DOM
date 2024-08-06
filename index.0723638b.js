4fdc3f9cb76c0440187c84c9d715db53ac68c300
my-git-username <118355685+my-git-username@users.noreply.github.com>
2024-08-06 14:23:56 +0300

    make build

---
+++ index.0723638b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth/2,n=e.clientHeight/2,i=t.getBoundingClientRect().height/2,l=t.getBoundingClientRect().width/2;t.style.top="".concat(n-i,"px"),t.style.left="".concat(c-l,"px");
+//# sourceMappingURL=index.0723638b.js.map
